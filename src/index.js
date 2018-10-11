67edd68c6ee2d9ac9edd6e1f3c65e31d47c8944d
David Gridley <davidgridley21@gmail.com>
2018-10-11 12:02:16 +0100

    started dogHouse

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // Create a function which stores a number. Return an inner function which accepts a number as an argument, adds it to the number in parent scope and returns the new total
 
-function increase(){
-
+function increase() {
   let number = 2;
 
-  function inner(newNumber){
+  function inner(newNumber) {
     number = newNumber + number;
     return number;
   }
@@ -12,11 +11,10 @@ function increase(){
   return inner;
 }
 
-function double(){
-
+function double() {
   let multiplier = 2;
 
-  function inner(newNumber){
+  function inner(newNumber) {
     number = newNumber * multiplier;
     return number;
   }
@@ -24,69 +22,88 @@ function double(){
   return inner;
 }
 
-function total(){
-
+function total() {
   let total = 0;
 
   function arrayTotal(arr) {
-    total = arr.reduce((acc, num) => acc + num )
-    return total
+    total = arr.reduce((acc, num) => acc + num);
+    return total;
   }
 
   return arrayTotal;
 }
 
-
 function gibberish() {
   let string = "";
 
   function inner(input) {
-    if (typeof input === 'object') {
-      string = string + input.join(' ') + "."
-      return string
+    if (typeof input === "object") {
+      string = string + input.join(" ") + ".";
+      return string;
     }
 
-    string = string + " " + input
+    string = string + " " + input;
 
-    return string
-    }
+    return string;
+  }
 
-    return inner;
+  return inner;
 }
 
 function mergeSort() {
   let arr = [];
 
   function inner(number) {
-    arr.push(number)
-    arr.sort((a,b) => b - a)
+    arr.push(number);
+    arr.sort((a, b) => b - a);
     return arr[0];
   }
 
   return inner;
 }
 
-function calculator (){
-  let total = 0
+function calculator() {
+  let total = 0;
 
-  function inner(n1,n2, operator) {
+  function inner(n1, n2, operator) {
     switch (operator) {
-      case "+" :
-        total += (n1 + n2)
+      case "+":
+        total += n1 + n2;
         break;
-      case "-" :
-        total +=  (n1 - n2)
+      case "-":
+        total += n1 - n2;
         break;
-      case "*" :
-        total +=   (n1 * n2)
+      case "*":
+        total += n1 * n2;
         break;
-      case "/" :
-        total +=   (n1 / n2)
+      case "/":
+        total += n1 / n2;
         break;
     }
-    return total
+    return total;
   }
-  return inner
+  return inner;
+}
+
+function dogHome() {
+  const dogObject = {};
+
+  const dogMethods = {
+    houseDog(name, breed, colour, location) {
+      if (dogObject[location]) {
+        dogObject[location].push({ name: name, breed: breed, colour: colour });
+      } else {
+        Object.assign(dogObject, {
+          [location]: [{ name: name, breed: breed, colour: colour }]
+        });
+      }
+    },
+    getDogsByLocation(location) {
+      return dogObject[location];
+    }
+  };
+
+  return dogMethods;
 }
 
 module.exports = {
@@ -95,5 +112,6 @@ module.exports = {
   total,
   gibberish,
   mergeSort,
-  calculator
-}
+  calculator,
+  dogHome
+};
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,14 @@
-const { increase, double, total, gibberish, mergeSort, calculator } = require('../src/index.js');
+const {
+  increase,
+  double,
+  total,
+  gibberish,
+  mergeSort,
+  calculator,
+  dogHome
+} = require("../src/index.js");
 
-test('increase', function(){
+test("increase", function() {
   const arg = 10;
   const inner = increase();
   const result = inner(arg);
@@ -10,7 +18,7 @@ test('increase', function(){
   expect(result).toBe(expected);
 });
 
-test('double', function(){
+test("double", function() {
   const arg = 10;
   const inner = double();
   const result = inner(arg);
@@ -20,48 +28,48 @@ test('double', function(){
   expect(result).toBe(expected);
 });
 
-test('total', function(){
+test("total", function() {
   const arr = [12, 14, 5, 2, 10];
   const arrayTotal = total();
   const result = arrayTotal(arr);
 
   const expected = 43;
   expect(result).toBe(expected);
-})
+});
 
-describe('gibberish', function(){
-  test('string passed in', function(){
-    const inner = gibberish()
-    const input = "hello"
-    const result = inner(input)
-    const expected = " hello"
-
-    expect(result).toBe(expected);
-  })
-
-  test('array passed in', function(){
-    const inner = gibberish()
-    const input = ["dog","cat","lamp"]
-    const result = inner(input)
-    const expected = "dog cat lamp."
+describe("gibberish", function() {
+  test("string passed in", function() {
+    const inner = gibberish();
+    const input = "hello";
+    const result = inner(input);
+    const expected = " hello";
 
     expect(result).toBe(expected);
-  })
+  });
 
-  test('array and string passed in', function(){
-    const inner = gibberish()
-    let input = ["dog","cat","lamp"]
-    let result = inner(input)
-    input = "man"
-    result = inner(input)
-    const expected = "dog cat lamp. man"
+  test("array passed in", function() {
+    const inner = gibberish();
+    const input = ["dog", "cat", "lamp"];
+    const result = inner(input);
+    const expected = "dog cat lamp.";
 
     expect(result).toBe(expected);
-  })
-})
+  });
 
-describe('mergeSort', function(){
-  test('passing in several numbers', function(){
+  test("array and string passed in", function() {
+    const inner = gibberish();
+    let input = ["dog", "cat", "lamp"];
+    let result = inner(input);
+    input = "man";
+    result = inner(input);
+    const expected = "dog cat lamp. man";
+
+    expect(result).toBe(expected);
+  });
+});
+
+describe("mergeSort", function() {
+  test("passing in several numbers", function() {
     const inner = mergeSort();
     inner(3);
     inner(4);
@@ -69,16 +77,50 @@ describe('mergeSort', function(){
     const result = inner(6);
     const expected = 6;
     expect(result).toBe(expected);
-  })
-})
+  });
+});
 
-test('calculator', function() {
+test("calculator", function() {
   const inner = calculator();
-  inner(5, 5, '+')
-  inner(10, 5, '-')
-  inner(10, 10, '*')
-  const result = inner(100,10,'/')
+  inner(5, 5, "+");
+  inner(10, 5, "-");
+  inner(10, 10, "*");
+  const result = inner(100, 10, "/");
   const expected = 125;
 
-  expect(result).toBe(expected)
-})
+  expect(result).toBe(expected);
+});
+
+describe("dogHome", function() {
+  let dogMethods;
+  beforeEach(() => {
+    dogMethods = dogHome();
+  });
+
+  test("use houseDog with single dog", function() {
+    dogMethods.houseDog("bonnie", "springer", "black and white", "marlborough");
+
+    const result = dogMethods.getDogsByLocation("marlborough");
+    const expected = [{
+      name: "bonnie",
+      breed: "springer",
+      colour: "black and white"
+    }];
+    expect(result).toEqual(expected);
+  });
+
+  // test("use houseDog with > 1 dog", function() {
+  //   const result = dogMethods.houseDog(
+  //     "bonnie",
+  //     "springer",
+  //     "black and white",
+  //     "marlborough"
+  //   );
+  //   const expected = {
+  //     marlborough: [
+  //       { name: "bonnie", breed: "springer", colour: "black and white" }
+  //     ]
+  //   };
+  //   expect(result).toEqual(expected);
+  // });
+});
